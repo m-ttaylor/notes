9ef3a9d93a6947e88c0d66cf71f09871d774277e
m-ttaylor <mttaylor@email.wm.edu>
2022-05-23 11:51:35 -0400

    Update to use mongo db

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
 const app = express()
+const Note = require('./models/note')
 
 app.use(express.json())
 app.use(express.static('build'))
-app.use(cors())
 
 let notes = [
   {
@@ -34,20 +34,15 @@ let notes = [
 ]
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => {
-    return (note.id === id)
-  })
-
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -66,24 +61,22 @@ const generateId = () => {
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing'})
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId()
-  }
+  })
 
-  notes = notes.concat(note)
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
